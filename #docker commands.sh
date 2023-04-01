810e2c4f64fd366c7e5d146b3bbb18acd19c042a
Sadiq Ali Khan <88760696+sadiqk2@users.noreply.github.com>
2023-04-01 14:23:13 +0530

    Update #docker commands.sh

--- #docker commands.sh
+++ #docker commands.sh
@@ -3,3 +3,10 @@
 sudo docker run -it --name sadiq -p 80:80  sadiqk1/assignment96 /bin/bash
 sudo docker commit f32d8a7938e2 assignment96
 https://medium.com/codex/push-docker-image-to-docker-hub-acc978c76ad
+
+
+sudo docker build ubuntuDocker
+sudo docker images
+sudo docker run -it --name yakdam -p 80:80 2e67faf3d459
+nano /var/www/html/index.html
+Customize html file
