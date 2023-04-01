71a76285d48fbb1fe32404eaaea534f7c20605e6
Sadiq Ali Khan <88760696+sadiqk2@users.noreply.github.com>
2023-04-01 11:41:13 +0530

    Add files via upload

---
+++ #docker commands.sh
@@ -0,0 +1,5 @@
+#docker commands
+
+sudo docker run -it --name sadiq -p 80:80  sadiqk1/assignment96 /bin/bash
+sudo docker commit f32d8a7938e2 assignment96
+https://medium.com/codex/push-docker-image-to-docker-hub-acc978c76ad
